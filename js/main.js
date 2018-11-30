a79a4fa461cc1965f020ee5caf6b117aa5c8b44f
Kseniya Maslak <Kseniya_Maslak@epam.com>
2018-11-30 17:14:23 +0300

    added separation into files

--- js/main.js
+++ js/main.js
@@ -1,49 +1,7 @@
 import '../sass/main.sass';
+import newsContainer from './newsContainer.js'
 
-const apiKey = '1d143d74c3f949b287751439d8842708';
+const API_KEY = '1d143d74c3f949b287751439d8842708';
 const newsElement = document.getElementById('news-container');
 
-class newsContainer {
-    constructor() {
-        this.channel = '';
-    }
-    getNews() {
-        [...newsElement.childNodes].forEach(el => el.remove());
-        const url = `https://newsapi.org/v2/top-headlines?sources=${
-            this.channel
-        }&apiKey=${apiKey}`;
-        this.loadNews(url,).then(data => {
-            const { status, articles, message, ...rest } = data;
-            if (status === 'ok') {
-                for (let article of articles) {
-                    newsElement.appendChild(this.createArticle(article));
-                }
-            } else {
-                const messageElement = document.createElement('h3');
-                messageElement.innerHTML = message;
-                newsElement.appendChild(messageElement);
-            }
-        });
-    }
-    async loadNews(url,) {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    }
-    setChannel(value,) {
-        this.channel = value;
-    }
-    createArticle(article,) {
-        const articleElement = document.createElement('article');
-        articleElement.innerHTML = `
-        <h1>${article.title}</h1>
-        ${article.author ? `<h4>By ${article.author}</h4>` : ''}
-        <h4>${article.publishedAt}</h4>
-        <h3>${article.description}</h3>
-        <p>${article.content}</p>
-        `;
-        return articleElement;
-    }
-}
-
-window.container = new newsContainer();
+window.container = new newsContainer(newsElement, API_KEY);
