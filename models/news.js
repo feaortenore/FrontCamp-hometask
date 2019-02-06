b63b497b1fce01dd9d039e37e104c7e36b83995a
Kseniya Maslak <Kseniya_Maslak@epam.com>
2019-02-06 20:31:13 +0300

    added authorization

--- models/news.js
+++ models/news.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/frontcamp', { useNewUrlParser: true });
 const newSchema = new mongoose.Schema({ title: String, description: String })
 module.exports = mongoose.model('News', newSchema)
--- routes/news.js
+++ routes/news.js
@@ -1,52 +1,57 @@
 const express = require('express');
 const router = express.Router();
-const News = require('../data/news');
+const News = require('../models/news');
 const createError = require('http-errors');
- 
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+  }
+
 /* GET news listing. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function (req, res, next) { // check for loggedIn for testing
     News.find({}, function (err, docs) {
-        if(!err){
+        if (!err) {
             res.send(docs);
         } else {
             console.log(err.message);
             next(createError(500));
         }
-      });
+    });
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
     News.findOne({ _id: req.params.id }, function (err, doc) {
-        if(!err){
+        if (!err) {
             res.send(doc);
         } else {
             console.log(err.message);
             next(createError(400));
         }
-      });
+    });
 });
 
-router.delete('/', function(req, res, next) {
+router.delete('/', function (req, res, next) {
     next(createError(406));
 });
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', ensureAuthenticated, function (req, res, next) {
     News.deleteOne({ _id: req.params.id }, function (err) {
-        if(!err){
+        if (!err) {
             res.sendStatus(200);
         } else {
             console.log(err);
             res.sendStatus(204);
         }
-      });
+    });
 });
 
-router.put('/', function(req, res, next) {
+router.put('/', ensureAuthenticated, function (req, res, next) {
     const body = req.body;
-    if(body._id) {
+    if (body._id) {
         const news = new News(body);
         News.update({ _id: body._id }, news, function (err, doc) {
-            if(!err){
+            if (!err) {
                 res.sendStatus(200);
             } else {
                 next(createError(501));
